463bb6ce3a728fc6184f6763371030d8a9b4d2ae
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-27 15:16:21 +0100

    create specific route for page where projects list is printed

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,18 @@
 <script>
-import axios from "axios";
-import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
   name: "App",
   components: {
-    ProjectCard,
-  },
-  data() {
-    return {
-      apiFirstUrl: "http://127.0.0.1:8000/api/projects",
-      lastPage: null,
-      projects: [],
-      linkPages: [],
-      currentPage: null
-    };
-  },
-  methods: {
-    getApi(url) {
-      axios.get(url).then((result) => {
-        this.projects = result.data.projects.data;
-        this.linkPages = result.data.projects.links;
-        this.lastPage = result.data.projects.last_page;
-      });
-    },
-    getPage(numberPage) {
-      axios.get(this.linkPages[numberPage].url).then((result) => {
-        this.projects = result.data.projects.data;
-        this.linkPages = result.data.projects.links;
-        this.currentPage = result.data.projects.current_page;
-      });
-    }
-  },
-  mounted() {
-    this.getApi(this.apiFirstUrl);
-  },
+  
+  }
 };
 </script>
 
 <template>
   <div class="container">
-    <div class="main-wrap">
-      <h1 class="title">LISTA PROGETTI</h1>
-      <router-view></router-view>
-      <div class="cards-container">
-          <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
-      </div>
-      <div class="pagination">
-        <button @click="getPage(1)"><i class="fa-solid fa-backward-step"></i></button>
-        <template v-for="page in linkPages" :key="page.label">
-          <button :class="{clicked: page.active}" v-if="!isNaN(page.label)" @click="getPage(parseInt(page.label))" >{{ page.label }}</button>
-        </template>
-        <button @click="getPage(lastPage)"><i class="fa-solid fa-forward-step"></i></button>
-      </div>      
-    </div>
+      <main>
+        <router-view></router-view>
+      </main>     
   </div>
 </template>
 
--- src/components/pages/Projects.vue
+++ src/components/pages/Projects.vue
@@ -1,12 +1,90 @@
 <script>
+import axios from "axios";
+import ProjectCard from "../ProjectCard.vue";
 export default {
-  name: 'Projects'
-
-}
+  name: "Projects",
+  components: {
+    ProjectCard,
+  },
+  data() {
+    return {
+      apiBaseUrl: "http://127.0.0.1:8000/api/projects",
+      lastPage: null,
+      projects: [],
+      linkPages: [],
+      currentPage: null,
+    };
+  },
+  methods: {
+    getApi(url) {
+      axios.get(url).then((result) => {
+        this.projects = result.data.projects.data;
+        this.linkPages = result.data.projects.links;
+        this.lastPage = result.data.projects.last_page;
+      });
+    },
+    getPage(numberPage) {
+      axios.get(this.linkPages[numberPage].url).then((result) => {
+        this.projects = result.data.projects.data;
+        this.linkPages = result.data.projects.links;
+        this.currentPage = result.data.projects.current_page;
+      });
+    },
+  },
+  mounted() {
+    this.getApi(this.apiBaseUrl);
+  },
+};
 </script>
-<template>
-  PROGETTI
-</template>
-<style>
 
+<template>
+  <h1 class="title">LISTA PROGETTI</h1>
+  <div class="cards-container">
+    <ProjectCard
+      v-for="project in projects"
+      :key="project.id"
+      :project="project"
+    />
+  </div>
+  <div class="pagination">
+    <button @click="getPage(1)">
+      <i class="fa-solid fa-backward-step"></i>
+    </button>
+    <template v-for="page in linkPages" :key="page.label">
+      <button
+        :class="{ clicked: page.active }"
+        v-if="!isNaN(page.label)"
+        @click="getPage(parseInt(page.label))"
+      >
+        {{ page.label }}
+      </button>
+    </template>
+    <button @click="getPage(lastPage)">
+      <i class="fa-solid fa-forward-step"></i>
+    </button>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+ .cards-container {
+    margin-top: 50px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  .pagination {
+    button {
+      padding: 10px 15px;
+      background-color: white;
+      border: 0;
+      margin-left: 5px;
+      border-radius: 5px;
+      color: #405189;
+      font-weight: 600;
+      &.clicked {
+        background-color: #add8e6;
+      }
+    }
+  }
 </style>
