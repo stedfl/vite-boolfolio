1060ed7f3568dc0258ba4e36f430a8db3124408a
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-31 15:42:05 +0100

    add layout to contacts page with form as vue component

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+  name: "ContactForm",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<template>
+  <form>
+    <div class="form-item">
+      <label for="name">Nome *</label>
+      <input type="text" id="name" placeholder="Scrivi qui il tuo nome" />
+    </div>
+    <div class="form-item">
+      <label for="email">E-mail *</label>
+      <input type="text" id="email" placeholder="Scrivi qui la tua mail" />
+    </div>
+    <div class="form-item">
+      <label for="object">Oggetto *</label>
+      <input
+        type="text"
+        id="object"
+        placeholder="Scrivi qui l'oggetto del messaggio"
+      />
+    </div>
+    <div class="form-item">
+      <label for="message">Messaggio *</label>
+      <textarea
+        name=""
+        id="message"
+        cols="30"
+        rows="10"
+        placeholder="Scrivi qui il messaggio"
+      ></textarea>
+    </div>
+    <button>Invia</button>
+  </form>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/partials/variables" as *;
+
+.form-item {
+  margin-bottom: 1rem;
+  label {
+    display: block;
+    margin-bottom: 0.3rem;
+    color: $secondary-logo-color;
+    font-weight: 500;
+  }
+
+  input,
+  textarea {
+    width: 450px;
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+  }
+
+  textarea::placeholder {
+    font-family: "Montserrat", sans-serif;
+    font-size: 13px;
+  }
+}
+
+button {
+  padding: 5px 10px;
+  background-color: $secondary-logo-color;
+  color: white;
+  font-family: "Montserrat", sans-serif;
+  border-radius: 5px;
+  border: 1px solid $secondary-logo-color;
+  font-size: 0.9rem;
+  &:hover {
+    background-color: lighten($secondary-logo-color, 10%);
+  }
+  &:active {
+    background-color: lighten($secondary-logo-color, 15%);
+    font-size: 1rem;
+  }
+}
+</style>
--- src/components/pages/Contacts.vue
+++ src/components/pages/Contacts.vue
@@ -1,12 +1,66 @@
 <script>
+import ContactForm from "../ContactForm.vue";
 export default {
-  name: 'Contacts'
-}
+  name: "Contacts",
+  components: {
+    ContactForm,
+  },
+};
 </script>
 
 <template>
-  <div class="container">CONTATTI</div>
+  <main>
+    <div class="container">
+      <div class="main-wrap">
+        <h1>
+          <span class="name title">Sdf</span>
+          <span class="job-left title">Web</span>
+          <span class="job-right title">Developer</span>
+        </h1>
+        <address>
+          <p>Strada Maggiore, 37 - Bologna</p>
+          <p>Telefono: +39 408-816-8765</p>
+          <a href="mailto:info-portfolio@sdf.it">info-portfolio@sdf.it</a>
+        </address>
+        <ContactForm />
+      </div>
+    </div>
+  </main>
 </template>
-<style>
 
+<style scoped lang="scss">
+@use "../../styles/partials/variables" as *;
+
+.container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.name,
+.job-right {
+  color: $secondary-logo-color;
+}
+
+.job-left {
+  color: $primary-logo-color;
+}
+
+.title {
+  margin-right: 10px;
+}
+
+h1 {
+  text-align: center;
+}
+
+address {
+  text-align: center;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+  & * {
+    margin-bottom: 0.4rem;
+  }
+}
 </style>
